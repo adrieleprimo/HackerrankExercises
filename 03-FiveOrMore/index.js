65f817633538a4d9ff6d0a953ce8ab09d7c86f9e
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-23 12:12:22 -0300

    in English

--- 03-FiveOrMore/index.js
+++ 03-FiveOrMore/index.js
@@ -1,16 +1,16 @@
-let precos = [100, 500, 100, 200, 50];
+let prices = [100, 500, 100, 200, 50];
 
-let soma = 0;
-let valor =100000;
-for(preco of precos){
-    soma+=preco;
+let sum = 0;
+let value =100000;
+for(price of prices){
+    sum+=price;
 }
-if(precos.length>=5){
-    for(menor of precos){
-        if(menor < valor){
-            valor = menor;
+if(prices.length>=5){
+    for(smaller of prices){
+        if(smaller < value){
+            value = smaller;
         }
     }
-    soma-=valor;
+    sum-=value;
 }
-console.log(soma);
+console.log(sum);

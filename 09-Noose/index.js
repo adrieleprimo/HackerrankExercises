29d5b47ce57f896fa02eb424a84b26a3eba2c648
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-24 22:50:07 -0300

    Exercise about Noose realized

---
+++ 09-Noose/index.js
@@ -0,0 +1,11 @@
+const palavra = "abelha";
+let  = 'a';
+let i = 0;
+let pontuacao = 0;
+for(letra of palavra){
+    if(palpite === letra){
+        pontuacao++;
+    }  
+    i++;
+}
+console.log(pontuacao)
